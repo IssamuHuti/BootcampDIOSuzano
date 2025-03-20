baaef19c4343a44d43994fbaf3357192daada73c
IssamuHuti <lucas.issamum@gmail.com>
2025-03-19 21:35:04 -0300

    atualização atividade bancária

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -6,6 +6,13 @@ menu = """
 
 => """
 
+menu2 = """
+[d] Depositar
+[e] Extrato
+[q] Sair
+
+=> """
+
 saldo = 0
 LIMITE = 500
 extrato = ""
@@ -13,8 +20,12 @@ numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-    opcao = input(menu)
-    print()
+    if numero_saques < 3:
+        opcao = input(menu)
+        print()
+    else:
+        opcao = input(menu2)
+        print()
 
     if opcao == "d":
         deposito = float(input('Informe o valor a depositar: '))
@@ -25,14 +36,26 @@ while True:
         else:
             print('Valor inválido!')
 
-    elif opcao == "s":
+    elif (opcao == "s") and (numero_saques < 3):
+        print(numero_saques)
         saque = float(input('Informe o valor a sacar: '))
-        if saque <= saldo:
+
+        if saque <= 0:
+            print('Valor inválido!')
+            continue
+
+        if (saque <= saldo) and (numero_saques < 3) and (saque <= LIMITE):
             saldo -= saque
             print(f'Saque de R$ {saque:.2f} realizado com sucesso!')
             extrato += f'Saque    {saque:10.2f}\n'
+        elif saque > LIMITE:
+            print(f'Pedido de saque excedeu o limite, solicite um valor abaixo do limite de {LIMITE:.2f}!')
+        elif numero_saques == 3:
+            print(f'Excedeu o limite de saques diários!')
         else:
             print('Não foi possível realizar o saque, reveja o saldo antes de realizar o saque novamente!')
+
+        numero_saques += 1
 
     elif opcao == "e":
         print('Extrato bancário\n')
